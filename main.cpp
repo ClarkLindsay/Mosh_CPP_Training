589d07be7559df2adce4d1d096c2d58fd7da9be6
ClarkLindsay <clark.lindsay@siu.edu>
2022-08-12 11:00:53 -0400

    Testing some very very basic stuff

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+  const double pi = 3.14;
+  pi = 0;
+  //std::cout << "a: " + std::to_string(a) << "\n";
+  //std::cout << "b: " + std::to_string(b) << "\n";
+  return 0;
+}
